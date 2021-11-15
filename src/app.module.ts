dc5a303593e409d4af0a8d6eae09479375927913
Ygrick <cujbaigor90@gmail.com>
2021-11-15 22:33:30 +0200

    dotenv, connection to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+require('dotenv').config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: ['**/*.entity{.ts,.js}'],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
